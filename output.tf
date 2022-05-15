9d2f11d74a0a1e631fadb87d5204830c3e8ffe6d
Jonggu Lee <jg.jake.lee@gmail.com>
2022-05-16 01:06:44 +0900

    Update output.tf

--- output.tf
+++ output.tf
@@ -3,17 +3,17 @@ output "Bastion_public_ip" {
 }
 
 output "scn_private_ip_1" {
-    value = aws_instance.test-blockchain-scn-1-pri-2a.public_ip
+    value = aws_instance.test-blockchain-scn-1-pri-2a.private_ip
 }
 
 output "scn_private_ip_2" {
-    value = aws_instance.test-blockchain-scn-2-pri-2b.public_ip
+    value = aws_instance.test-blockchain-scn-2-pri-2b.private_ip
 }
 
 output "scn_private_ip_3" {
-    value = aws_instance.test-blockchain-scn-3-pri-2c.public_ip
+    value = aws_instance.test-blockchain-scn-3-pri-2c.private_ip
 }
 
 output "scn_private_ip_4" {
-    value = aws_instance.test-blockchain-scn-4-pri-2d.public_ip
+    value = aws_instance.test-blockchain-scn-4-pri-2d.private_ip
 }
