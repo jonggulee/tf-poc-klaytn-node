527712f57ea5f22e8d799ec78ee090ff8a72a6eb
Jonggu Lee <jg.jake.lee@gmail.com>
2022-05-15 19:47:10 +0900

    Update data.tf

--- data.tf
+++ data.tf
@@ -14,7 +14,22 @@ data "aws_ami" "amzn2" {
   owners = ["amazon"] # Canonical
 }
 
-# variable "aws_region" {
-#   description = "AWS region"
-#   default = "ap-northeast-2"
-# }
+variable "aws_region" {
+  description = "AWS region"
+  default = "ap-northeast-2"
+}
+
+variable "scn_type" {
+  description = "SCN instance type"
+  default = "t3.xlarge"
+}
+
+variable "scn_volume_type" {
+  description = "SCN volume type"
+  default = "gp3"
+}
+
+variable "scn_volume_size" {
+  description = "SCN volume type"
+  default = "30"
+}
