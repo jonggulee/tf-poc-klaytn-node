9c36d02a781bec04668b511a0106add31b4abb77
Jonggu Lee <jg.jake.lee@gmail.com>
2022-05-16 00:43:54 +0900

    Add output scn private ip

--- output.tf
+++ output.tf
@@ -2,18 +2,18 @@ output "Bastion_public_ip" {
     value = aws_instance.test-blockchain-bastion-pub-2c.public_ip
 }
 
-# output "scn_private_ip_1" {
-#     value = aws_instance.test-blockchain-scn-1-pri-2a.public_ip
-# }
+output "scn_private_ip_1" {
+    value = aws_instance.test-blockchain-scn-1-pri-2a.public_ip
+}
 
-# output "scn_private_ip_2" {
-#     value = aws_instance.test-blockchain-scn-2-pri-2b.public_ip
-# }
+output "scn_private_ip_2" {
+    value = aws_instance.test-blockchain-scn-2-pri-2b.public_ip
+}
 
-# output "scn_private_ip_3" {
-#     value = aws_instance.test-blockchain-scn-3-pri-2c.public_ip
-# }
+output "scn_private_ip_3" {
+    value = aws_instance.test-blockchain-scn-3-pri-2c.public_ip
+}
 
-# output "scn_private_ip_4" {
-#     value = aws_instance.test-blockchain-scn-4-pri-2d.public_ip
-# }
+output "scn_private_ip_4" {
+    value = aws_instance.test-blockchain-scn-4-pri-2d.public_ip
+}
